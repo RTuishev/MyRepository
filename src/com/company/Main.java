c8b4fe5bfdc6c11778d9f4f3e219202eb4e0b645
ramis <faustramis@gmail.com>
2021-03-23 20:28:27 +0300

    1-5 задания

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,22 +9,27 @@ public class Main {
         System.out.println("После: " + a + "а");
         System.out.println();
         String b = "Много $";// Второе задание
-        System.out.println(b);
+        System.out.println("До: " + b);
         String c = b.substring(0, 6);
-        System.out.println(c + "&");
+        System.out.println("После: " + c + "&");
         System.out.println();
-        String value = "осталось много времени до нового года".replace("много времени", "4 месяца");// Третье задание
-        System.out.println(value);
+        String value0 = "осталось много времени до нового года";
+        System.out.println("До: " + value0);
+        value0 = value0.replace("много времени", "4 месяца");// Третье задание
+        System.out.println("После: " + value0);
         System.out.println();
-        String e = value.substring(8, 10);// Четвертое задание
+        System.out.println("До: " + value0);
+        String e = value0.substring(8, 10);// Четвертое задание
         int i = Integer.parseInt(e.trim());
         int j = i - 1;
         String f = Integer.toString(j);
-        String value1 = value.replace("4", f);
-        System.out.println(value1);
+        String value1 = value0.replace("4", f);
+        System.out.println("После: " + value1);
         System.out.println();
-        String value2 = "Сосед и децибелы".replace(" и децибе", "и имбеци");// Пятое задание
-        System.out.println(value2);
+        String value2 = "Сосед и децибелы";// Пятое задание
+        System.out.println("До: " + value2);
+        value2 = value2.replace(" и децибе", "и имбеци");
+        System.out.println("После: " + value2);
 
 
     }
