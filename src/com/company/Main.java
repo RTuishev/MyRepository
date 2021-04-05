2ac275396ccc1cd4f358078987e2037556c273a9
ramis <faustramis@gmail.com>
2021-04-05 23:21:40 +0300

    Ввод с клавиатуры

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,11 @@
 package com.company;
 
-import java.lang.reflect.Array;
+import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws Exception {
 //        String Link = "Шаурмяу";// Первое задание
 //        System.out.println("До: " + Link);
 //        String a = Link.substring(0, 5);
@@ -34,24 +35,78 @@ public class Main {
 //        System.out.println("После: " + value2);
 
 
-        Cycle();
+//        Cycle();
 
+        // Задание с вводом с клавиатуры
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Введите ФИО: ");
+        String q1 = keyboard.nextLine();
+        System.out.println("Дату рождения: ");
+        String q2 = keyboard.nextLine();
+        System.out.println("ФИО: " + q1 + ", г.р.: " + q2);
+        System.out.println();
+        int f = 3;
+//        for (int i = 0; i < 3; i++) {
+            do {
+            f--;
+            System.out.println("Введите первое число больше нуля: ");
+            String a1 = keyboard.nextLine();
+            System.out.println("Введите второе число больше нуля: ");
+            String a2 = keyboard.nextLine();
 
-    }
-
-    public static void Cycle() {
-        for (int i = 0; i < 5; i++) {
+            System.out.println();
             try {
-                int a = i + 1;
-                if (a == 3)
-                    throw new Exception("Number " + a + " is exception");
-                System.out.println(a);
-            } catch (Exception e) {
+                int b1 = Integer.parseInt(a1);
+                int b2 = Integer.parseInt(a2);
+                if (b1 == 0 && b2 == 0) {
+                    if (f != 0) {
+                        System.out.println("Попробуйте снова. Осталось попыток " + f);
+                        System.out.println();
+                    } else {
+                        System.out.println();
+                        System.out.println("Попытки исчерпаны. Попробуйте завтра.");
+                    }
+
+                } else {
+                    int s1 = b1 + b2;
+                    System.out.println("Результат: " + b1 + " + " + b2 + " = " + s1);
+                    break;
+                }
+
+            } catch (NumberFormatException e) {
                 e.printStackTrace();
+                if (f != 0) {
+                    System.out.println("Попробуйте снова. Осталось попыток " + f);
+                    System.out.println();
+                } else {
+                    System.out.println();
+                    System.out.println("Попытки исчерпаны. Попробуйте завтра.");
+                }
+
             }
 
+
+
+
         }
+            while (f > 0);
+
 
     }
 
+//    public static void Cycle() {
+//        for (int i = 0; i < 5; i++) {
+//            try {
+//                int a = i + 1;
+//                if (a == 3)
+//                    throw new Exception("Number " + a + " is exception");
+//                System.out.println(a);
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//
+//        }
+//
+//    }
+//
 }
